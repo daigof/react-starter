29c6ad932fdb081b864ec10110a8d7ea7acf12a1
Fortes Diego <daigof@gmail.com>
2016-01-12 15:30:36 -0300

    initial

---
+++ js/entry.js
@@ -0,0 +1,17 @@
+ // require( './module1' );
+ // require( './module2' );
+
+ var React = require( 'react' );
+ var ReactDOM = require( 'react-dom' );
+
+ var HelloMessage = React.createClass( {
+   render: function() {
+     return (
+       <div> Hello {this.props.name} </div>
+     );
+   }
+ } );
+
+ ReactDOM.render( <HelloMessage name = "Diego" /> ,
+   document.getElementById( 'app' )
+ );
---
+++ js/module1.js
@@ -0,0 +1,4 @@
+var $ = require('jquery');
+console.log('m1');
+
+$('h1').fadeOut(5000);
